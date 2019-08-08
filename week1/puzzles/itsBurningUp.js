363881daf3abf0d674c556839e828ffa6f961927
mingsterism <ming.k@hotmail.com>
2019-08-08 18:00:29 +0800

    Initial commit

---
+++ week1/puzzles/itsBurningUp.js
@@ -0,0 +1,11 @@
+/**
+ * You and your family is visting a new country. You are reading the temperature
+ * in kelvin. But no one in your family understands this weird format as your
+ * family only reads tempateure only in celcius. Convert the temperature to
+ * celcius.
+ *
+ * */
+
+// Start here ----
+//  var kelvinTemperature = 50.9
+//  var celcius = ??
